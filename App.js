8113c204a2794fcb543ead0e6741d581c48e2fb5
Dabasis Biswas <97307213+Dabasis@users.noreply.github.com>
2022-01-18 09:14:59 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Card from './components/Card';
+
+function App() {
+  return <div>
+    <h1 className="headingstyle ">God is good</h1>
+    <Card />
+    <Card />
+    <Card />
+
+  </div>
+}
+
+export default App;
